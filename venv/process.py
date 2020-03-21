26fb697d555b1b8e5e04f9a753499963c190982f
R3kki <richie.parkie@gmail.com>
2020-03-21 02:00:05 -0400

    regex now removes whitespaces

--- venv/process.py
+++ venv/process.py
@@ -5,7 +5,6 @@ from ProcessData import ProcessData as preprocess
 
 def CSV_Reader(str):
     ''' Reads CSV and returns a pandas dataframe object '''
-
     data = pd.read_csv(str)
     text = data["text"]
     label = data["class"]
@@ -17,8 +16,8 @@ def CSV_Reader(str):
         single_review_words = text[i].split()
         for word in single_review_words:
             # clean up words. i.e. "Fun should be fun
-            clean_word = re.sub('[^A-Za-z]+', ' ', word)
-            clean_word = re.sub('[^A-Za-z]+', '', word) # seperate i.e. pizza!!Tony -> pizzaTony
+            clean_word = re.sub('[^A-Za-z]+', ' ', word) # seperate i.e. pizza!!Tony -> pizzaTony
+            clean_word = re.sub('[^\S]', '', clean_word) # selects non-whitespace
             # all lowercase
             clean_word = clean_word.lower()
             srw.append(clean_word)
@@ -39,7 +38,6 @@ def DF_to_CSV(df, new_name):
     new_train_file = "./../" + new_name
     df.to_csv(new_train_file, encoding='utf-8', index=False)
 
-
 def DF_to_PKL(df, new_name):
     ''' Avoid reading the data each time to process the original .csv.
         Returns new object removes special characters, whitespace, and puts into a dataframe object '''
@@ -57,16 +55,16 @@ def Get_Original_CSV(file_name):
     DF_to_CSV(df, "clean_train.csv")
     DF_to_PKL(df, "clean_train.pkl")
 
-
 def main():
     ''' Run Once: get pandas object (pickle) as a new file. then comment out '''
     # Get_Original_CSV("./../train2.csv") # outputs: clean_train.csv, clean_train.pkl
-    ''' Comment out above '''
+    ''' Comment-out above after use '''
 
     ''' Start Pre-processing methods '''
     clean_df = Get_DF_from_PKL("clean_train.pkl")
     m = preprocess(clean_df)
-    stop_df = m.removeStopWords()
+    stop_df = m.removeStopWords() # takes a long time -> comment out
+    ''' Comment-out above '''
     DF_to_CSV(stop_df, "stop_df.csv")
 
 main()
