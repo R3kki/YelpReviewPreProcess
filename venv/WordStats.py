4440452ed8881ae7b65f7e366b4e7f7f63f51dd4
R3kki <richie.parkie@gmail.com>
2020-03-23 12:07:07 -0400

    Train using Test data

--- venv/WordStats.py
+++ venv/WordStats.py
@@ -1,15 +1,18 @@
 ''' Data collection techniques given a dataframe object'''
 import pandas as pd
 import operator
+from ProcessData import ProcessData as process
 
 class WordStats:
-    def __init__(self, data):
+    def __init__(self, data, *args):
         self.data = data
         self.dict = {}
 
+        obj = process(data)
+
         ''' create dictionary '''
         id = self.data["ID"]
-        label = self.data["class"]
+        if len(args) < 1: label = data["class"]
         text = self.data["words"]
 
         words = []
@@ -17,49 +20,8 @@ class WordStats:
             text_row = []
             for j in range(len(text[i])):
                 # create dictionary
-                w = doStemming(text[i][j])
+                w = obj.doStemming(text[i][j])
                 self.dict[w] = self.dict.get(w, 0) + 1
-
-        doStemming()
-
-    def doStemming(self, word):
-        ''' Preform Porter Stemming Algorithm on the word
-        w + consonant (not s) + s => w + consonant
-        w + ies (not preceeded by e or a) => w + y
-        w + ing and length > 4 => w
-        w + consonant + ed and length > 4 => w + consonant
-        '''
-        word = self.word
-        vowels = "aeiou"
-        l = len(word)
-        cut = 1
-        if word[l-1] == 's':
-            if not word[l-2] == None and word[l-2] not in vowels and not(word[l-2] == 's') :
-                word = word[0:l-1]
-        if l > 4:
-            if word[l-3:l] == 'ies' and not not(word[l-4] == 'e' or word[l-4] == 'a'):
-                word = word[0 : l-3] + 'y'
-            elif word[l-3:l] == 'ies':
-                word = word[0 : l-3]
-            elif word[l-2:l] == 'ed' and word[l-3] not in vowels:
-                word = word[0 : l-2]
-
-        # m = 0 # [C](VC)^m[V]
-        # prev = 'c'
-        # # get M
-        # for letter in word:
-        #     if letter in vowels:
-        #         prev = 'v'
-        #     else:
-        #         if prev == 'v':
-        #             m = m + 1
-        #         prev = 'c'
-
-
-
-
-        return word
-
 
     def getDF(self):
         ''' Returns dataframe object -> type: dictionary '''
--- venv/process.py
+++ venv/process.py
@@ -4,11 +4,11 @@ import pickle
 from ProcessData import ProcessData as preprocess
 from WordStats import WordStats as stats
 
-def CSV_Reader(str):
+def CSV_Reader(str, *args):
     ''' Reads CSV and returns a pandas dataframe object '''
     data = pd.read_csv(str)
     text = data["text"]
-    label = data["class"]
+    if len(args) < 1: label = data["class"]
     id = data["ID"]
 
     words = []
@@ -18,18 +18,24 @@ def CSV_Reader(str):
         for word in single_review_words:
             clean_word = re.sub('[^A-Za-z]+', ' ', word) # no special char i.e. pizza!!Tony -> pizzaTony
             clean_word = re.sub('[^\S]', '', clean_word) # selects non-whitespace
-            # all lowercase
-            clean_word = clean_word.lower()
+            clean_word = clean_word.lower() # all lowercase
             if len(clean_word) > 2: # avoid adding "", single characters, double characters
                 srw.append(clean_word)
         words.append(srw)
 
     tupled_words = [tuple(word) for word in words]
-    df = {
-        'ID': id,
-        'class': label,
-        'words': tupled_words
-    }
+    if len(args) < 1: # training data
+        df = {
+            'ID': id,
+            'class': label,
+            'words': tupled_words
+        }
+    else:
+        df = {
+            'ID': id,
+            'words': tupled_words
+        }
+
     df = pd.DataFrame(df)
 
     return df
@@ -51,17 +57,28 @@ def Get_DF_from_PKL(new_name):
     train_data = pickle.load(open(pkl_name, "rb"))
     return train_data
 
-def Get_Original_CSV(file_name):
-    df = CSV_Reader(file_name)
-    DF_to_CSV(df, "01_clean_train.csv")
-    DF_to_PKL(df, "01_clean_train.pkl")
+def Get_Original_CSV(file_name, *args):
+    if len(args) >= 1:
+        df = CSV_Reader(file_name, True)
+        DF_to_CSV(df, "01_clean_test.csv")
+        DF_to_PKL(df, "01_clean_test.pkl")
+    else:
+        df = CSV_Reader(file_name)
+        DF_to_CSV(df, "01_clean_train.csv")
+        DF_to_PKL(df, "01_clean_train.pkl")
 
-def Basic_Stop_Words(pkl_name):
+def Basic_Stop_Words(pkl_name, *args):
     clean_df = Get_DF_from_PKL(pkl_name)
     m = preprocess(clean_df)
-    stop_df = m.removeStopWords("./../stop_words.lst")
-    DF_to_PKL(stop_df, "02_stop_df.pkl")
-    DF_to_CSV(stop_df, "02_stop_df.csv")
+    if len(args) >= 1:
+        stop_df = m.removeStopWords("./../stop_words.lst", args[0])
+    else:
+        stop_df = m.removeStopWords("./../stop_words.lst")
+
+    full_name = re.split(r"\.", pkl_name)
+    f_name = full_name[0] + "_stop_df"
+    DF_to_PKL(stop_df, f_name + ".pkl")
+    DF_to_CSV(stop_df, f_name + ".csv")
 
 def Remove_Infrequent_Words(freq, pkl_name):
     basic_stop_df = Get_DF_from_PKL(pkl_name)
@@ -82,28 +99,30 @@ def Remove_Infrequent_Words(freq, pkl_name):
     DF_to_PKL(stop_df, f_name+".pkl")
     DF_to_CSV(stop_df, f_name+".csv")
 
-
 def main():
     ''' Data Preprocessing Pipeline '''
 
     ''' 0. Given the original dataset '''
-    # Get_Original_CSV("./../train2.csv") # outputs: 01_clean_train.csv, 01_clean_train.pkl
+    # Get_Original_CSV("./../train2.csv", False) # outputs: 01_clean_train.csv, 01_clean_train.pkl
     ''' Comment-out above after use '''
 
-    """ 1. Removing Words with Basic Stop Word List """
-    # Basic_Stop_Words("01_clean_train.pkl") # outputs: 02_stop_df.csv, 02_stop_df.pkl
+    ''' 0.5 Get Test Stop words, frequencies too '''
+    # Get_Original_CSV("./../test2.csv", True) # outputs: 01_clean_test.csv, 01_clean_test.pkl
     ''' Comment-out above after use '''
 
-    """ 2. Remove words with frequency less than 10  """
+    """ 1. Test Data: Removing Words with Basic Stop Word List """
+    # Basic_Stop_Words("01_clean_test.pkl", True) # outputs: 01_clean_test_stop_df.csv, 01_clean_test_stop_df.pkl
+    ''' Comment-out above after use '''
+
+    basic_stop_df = Get_DF_from_PKL("01_clean_test_stop_df.pkl")
+    s = stats(basic_stop_df, True)
+    s.getDictionary()  # returns dictionary
+    basic_stop_df = s.getDF()
+    print(basic_stop_df)
+
+
+    """ 2. Test Data: Remove words with frequency less than 10  """
     # Remove_Infrequent_Words(10, "02_stop_df.pkl") # outputs: 02_stop_df_rem_freq_10.csv, 02_stop_df_df_rem_freq_10.pkl
     ''' Comment-out above after use '''
-
-    train_data = pickle.load(open("./../02_stop_df_rem_freq10.pkl", "rb"))
-    words = train_data["words"]
-    print(len(words))
-
-
-
-
 
 main()
